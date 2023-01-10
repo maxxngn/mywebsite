c469a187eb4e621a3b3ce3b3a31b9ff14938d800
maxxngn <118426016+maxxngn@users.noreply.github.com>
2023-01-10 14:47:23 +0700

    lesson19
    Jquery weather demo

---
+++ JQUERY/js/app.js
@@ -0,0 +1,7 @@
+$.get("https://api.openweathermap.org/data/2.5/weather?q=Hanoi&appid=96381a872b1b405c5bf83b2ed63d9561",
+function (api) {
+    $('.temp').html('Local Temp: ' + api.main.temp + 'C');
+    $('.city').html('City: ' + api.name);
+    $('.description').html('Description: ' + api.weather[0].description);
+    $('.icon img').attr('src','https://openweathermap.org/img/wn/' + api.weather[0].icon + '.png');
+});
